755865562a946d1b67214d61ede091089d1dbdec
Bryan Romero <bryanromerou@gmail.com>
2020-12-19 23:02:45 -0800

    added some comments

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -4,27 +4,37 @@ import Row from './Row';
 function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
 
 const Graph = () => {
-    const [input, setInput] = useState("");
-    const [change, setChange] = useState(true);
-    const [arrOfNums, setArrOfNums] = useState([]);
-    const [rows, setRows] = useState([]);
 
+
+    const [input, setInput] = useState("");//Tracking the input of the user
+    const [change, setChange] = useState(true);// Checks for change in array if there is a change it will trigger to rerender rows 
+    const [arrOfNums, setArrOfNums] = useState([]);// Integer Holder of all numbers
+    const [rows, setRows] = useState([]);//Component holder for all of my row components
+    const [largestNum, setLargestNum] = useState(1);//Tracking the greatest number for the height of the rows
+
+
+    //This function is ran when we have a change in the "change state"
     useEffect(()=>{
-        console.log("a number has been added to the array");
         const newRows = arrOfNums.map((elem,index)=>{
-            return <Row key = {index} number={elem}/>
+            return <Row key = {index} number={elem} largestNum={largestNum}/>
         });
         setRows(newRows);
     },[change])
 
     const submitHandler = (e) =>{
+        //Prevents a rerender that would lose data info
         e.preventDefault();
 
-        if(isNumber(input)){
+        //Will Store Number
+        const number = parseInt(input);
+        if(isNumber(input)){//Checks if it is a number and if it is it adds it to the array
             const newArr = arrOfNums;
-            newArr.push(parseInt(input));
+            newArr.push(number);
             setArrOfNums(newArr);
             setChange(!change);
+            if(number>largestNum){
+                setLargestNum(number);
+            }
         }else{
             console.log("That is not a number try again")
         }
@@ -35,14 +45,22 @@ const Graph = () => {
         setInput(e.target.value)
     }
 
+    // const sorter = () =>{
+    //     for (let index = 0; index < 10; index++) {
+    //         const element = array[index];
+            
+    //     }
+    // }
+
     return (
         <div>
             <form onSubmit={submitHandler}>
                 <input type="text" value={input} onChange={inputHandler}/>
                 <input type="submit" name="Submit"/>
-                {/* <button onClick = {submitHandler}>Press me</button> */}
             </form>
-            {rows}
+            <div className="all_rows">
+                {rows}
+            </div>
         </div>
     );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Row = (props) => {
+    
+    // Will Adjust the height according to the highest number the max number is 500 px
+    const height = 500*(props.number)/props.largestNum;
+
     return (
-        <div>
+        <div className="row" style={{width:"10px",height:`${height}px`}}>
             <h1>{props.number}</h1>
         </div>
     );
